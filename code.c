38a09a205c87d72733a0edfdeeccab05306be44b
Soumya Kanti Saha <sks19ms060@iiserkol.ac.in>
2023-08-29 23:21:04 +0530

    updated the code

--- code.c
+++ code.c
@@ -5,12 +5,26 @@
 int Lx,Ly,N_sites;
 int J=1;
 int state_arr[5000];
+int nbrarr[5000][4];
+float T;
+int L;
 
 void init_state()
 {
     for(int i=0;i<N_sites;i++)
     {
         state_arr[i]=0;
+    }
+}
+
+void init_ordered_state()
+{
+    for(int i=0;i<Lx;i++)
+    {
+        for(int j=0;j<Ly;j++)
+        {
+            state_arr[i*Ly+j]=1;
+        }
     }
 }
 void init_randomize_state()
@@ -68,24 +82,93 @@ float ord_param()
         {
             rho_y+=state_arr[i*Ly+j];
         }
-        m+=abs(rho_y/Lx - 0.5);
+        m+=fabs(rho_y/Lx - 0.5);
+        //printf("%f\n",abs(-2));
     }
     return 2*m/Ly;
 }
 
+float H()
+{
+    float E=0;
+    for(int i=0;i<N_sites;i++)
+    {
+        for(int j=0;j<4;j++)
+            E+=state_arr[i]*state_arr[nbrarr[i][j]];
+    }
+    return -J*E;
+}
+
+
+float MC_update(float E)
+{
+    float beta=1/T;
+    int i=(int)((float)rand()/RAND_MAX*N_sites);
+    if(state_arr[i])
+    {
+        int j=(int)((float)rand()/RAND_MAX*4);
+        if(!state_arr[nbrarr[i][j]])
+        {
+            float delE = 0;
+            for(int k=0;k<4;k++)
+                delE+= state_arr[nbrarr[i][k]] - state_arr[nbrarr[nbrarr[i][j]][k]];
+            delE=2*J*(delE+1);
+
+            if(delE<=0 || ((float)rand()/RAND_MAX)<exp(-beta*delE))
+            {
+                E+=delE;
+                state_arr[i]=0;
+                state_arr[nbrarr[i][j]]=1;
+            }
+        }
+    }
+    return E;
+}
+
+
+
+void get_data()
+{
+    int Trlax=10000000;
+    float E=H();
+    float m_mean=0;
+    float m2_mean=0;
+    float m4_mean=0;
+    int N_steps=10000000;
+    srand(99820);
+    for(int i=0;i<Trlax;i++) //reach steady state
+        for(int j=0;j<N_sites;j++)
+            E=MC_update(E);
+    printf("steady state reached, ord param =%f\n",ord_param());
+    for(int i=0;i<N_steps;i++)
+    {
+        for(int j=0;j<N_sites;j++)
+            E=MC_update(E);
+        //printf("MC step %d\n",i);
+        float m=ord_param();
+        m_mean+=m;
+        m2_mean+=m*m;
+        m4_mean+=m*m*m*m;
+    }
+    float data_arr[]={m_mean/N_steps,m2_mean/N_steps,m4_mean/N_steps};
+    printf("m=%f\nm2=%f\nm4=%f",data_arr[0],data_arr[1],data_arr[2]);
+    //return data_arr;
+}
+
 int main()
 {
-    int L;float T;
-    scanf("%d",&L);
+    scanf("%d %f",&L,&T);
     Lx=L;Ly=2*L;N_sites=Lx*Ly;
-
-    int nbrarr[N_sites][4];
     nbr2D(nbrarr);
+
     init_state();
     init_randomize_state();
 
+    printf("ord param of initial state = %f\n",ord_param());
+    get_data();
+    //float* data_arr=get_data();
 
-
+    //printf("m_mean = %f\nm2_mean = %f\nm4_mean = %f",data_arr[0],data_arr[1],data_arr[2]);
     return 0;
 }
 
